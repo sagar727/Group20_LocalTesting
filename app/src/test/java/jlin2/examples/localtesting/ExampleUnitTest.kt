939aec99ef4679c2401a94b71ce7535c3fd8088c
Sagar727 <sagar727@gmail.com>
2023-11-27 16:31:15 -0500

    starter code commit

---
+++ app/src/test/java/jlin2/examples/localtesting/ExampleUnitTest.kt
@@ -0,0 +1,26 @@
+package jlin2.examples.localtesting
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+
+
+    @Test
+    fun correctEmailAddressFormat(){
+        val result = EmailValidator.isValidEmail("abc@123.com")
+        assertEquals(false,result)
+    }
+}
